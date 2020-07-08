a49e67dd92959ee343ce35d8d0b67b157eaa7353
Yuhei-21 <67134329+Yuhei-21@users.noreply.github.com>
2020-07-08 20:28:42 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'chatspace20201021'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspce20201021'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace20201021'
 end
